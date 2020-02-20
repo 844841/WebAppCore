c29635b08bb0f577331ceadd9fe414cdf61dfd49
B.Supraja <41323544+844841@users.noreply.github.com>
2020-02-20 17:42:07 +0530

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppCoreMVCDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
